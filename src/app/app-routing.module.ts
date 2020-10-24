07dea75679e2ea69f228161aa60899c7bca1ed0d
Julia Dimasi <julia.dimasi@gmail.com>
2020-10-24 20:16:32 -0300

    avance la morenita

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { TipsComponent } from './tips/tips.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo:"inicio"
+  },
+  {
+    path:"inicio",
+    component: InicioComponent
+  },
+  {
+    path:"quienes-somos",
+    component: QuienesSomosComponent
+  },
+  {
+    path:"tips",
+    component: TipsComponent
+  },
+  {
+    path:"catalogo",
+    component: CatalogoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ContenidoComponent } from './contenido/contenido.component';
 import { InicioComponent } from './inicio/inicio.component';
-
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { FooterComponent } from './footer/footer.component';
+import { TipsComponent } from './tips/tips.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
 
 //agregados
@@ -19,15 +21,17 @@ import { InicioComponent } from './inicio/inicio.component';
     AppComponent,
     BannerComponent,
     NavbarComponent,
-    ContenidoComponent,
     InicioComponent,
-  
+    QuienesSomosComponent,
+    FooterComponent,
+    TipsComponent,
+    CatalogoComponent,
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule,
-  
-
+    IvyCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
