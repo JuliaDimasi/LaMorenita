9e03421e588b15888e648209e91c39309a4464c0
Julia Dimasi <julia.dimasi@gmail.com>
2020-10-24 20:14:44 -0300

    avances la morenita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+
+
+//agregados
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
+    NavbarComponent,
+    ContenidoComponent,
+    InicioComponent,
   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
